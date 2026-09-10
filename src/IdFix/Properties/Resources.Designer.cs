ba3a3a50f24502765a7b612dfcefa30e29017078
Vincenzo Sciarra <17055610+reikor0x@users.noreply.github.com>
2026-09-10 16:04:02 +0200

    Retargeted the application to .NET Framework 4.8

--- src/IdFix/Properties/Resources.Designer.cs
+++ src/IdFix/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace IdFix.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -96,7 +96,7 @@ namespace IdFix.Properties {
         ///&lt;country&gt;&lt;name&gt;Âland Islands&lt;/name&gt;&lt;alpha2&gt;AX&lt;/alpha2&gt;&lt;alpha3&gt;ALA&lt;/alpha3&gt;&lt;numeric&gt;248&lt;/numeric&gt;&lt;/country&gt;
         ///&lt;country&gt;&lt;name&gt;Albania&lt;/name&gt;&lt;alpha2&gt;AL&lt;/alpha2&gt;&lt;alpha3&gt;ALB&lt;/alpha3&gt;&lt;numeric&gt;8&lt;/numeric&gt;&lt;/country&gt;
         ///&lt;country&gt;&lt;name&gt;Algeria&lt;/name&gt;&lt;alpha2&gt;DZ&lt;/alpha2&gt;&lt;alpha3&gt;DZA&lt;/alpha3&gt;&lt;numeric&gt;12&lt;/numeric&gt;&lt;/country&gt;
-        ///&lt;country&gt;&lt;name&gt;American Samoa&lt;/name&gt;&lt;alpha2&gt;AS&lt;/alpha2&gt;&lt;alpha3&gt;ASM&lt;/alpha3&gt;&lt;nu [rest of string was truncated]&quot;;.
+        ///&lt;country&gt;&lt;name&gt;American Samoa&lt;/name&gt;&lt;alpha2&gt;AS&lt;/alpha2&gt;&lt;alpha3&gt;ASM&lt;/alpha [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string ISO3166 {
             get {
